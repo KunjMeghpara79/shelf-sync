6b946befb31b952852fa62be879e01642cd91a97
Kunj Meghapara <kunjmeghapara777@gmail.com>
2026-08-20 14:21:59 +0530

    starting the project

---
+++ src/test/java/com/example/ShelfSync/ShelfSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShelfSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
