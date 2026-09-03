e392de653fcb36b8a39207b7b296f439de279fbc
Kunj Meghapara <kunjmeghapara777@gmail.com>
2026-09-03 11:21:51 +0530

    ..

--- src/main/java/shelfsync/security/JwtAuthenticationFilter.java
+++ src/main/java/shelfsync/security/JwtAuthenticationFilter.java
@@ -38,8 +38,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
-
-
             final String authHeader = request.getHeader("Authorization");
             final String jwt;
             final String username;
